27cc641f845bde3e894e3c5a86d62027efacc307
SAEMI-wp <saisaisaida12@gmail.com>
2025-07-23 10:04:55 +0900

    Update settings.py
    DB연결 방식 변경

--- techave_kintai/settings.py
+++ techave_kintai/settings.py
@@ -26,17 +26,13 @@ DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = ['*']
 
 # Djangoアプリ設定
-INSTALLED_APPS = [
-    'django.contrib.admin',         # 管理ページ
-    'django.contrib.auth',          # 認証システム
-    'django.contrib.contenttypes',  # コンテンツタイプ
-    'django.contrib.sessions',      # セッション管理
-    'django.contrib.messages',      # メッセージフレームワーク
-    'django.contrib.staticfiles',   # 静的ファイル管理
-    'crispy_forms',                 # フォームレンダリング (attendance/forms.pyで使用)
-    'crispy_bootstrap5',            # Bootstrap5 スタイル
-    'attendance',                   # 勤怠管理メインアプリ
-]
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
+}
 
 # ミドルウェア設定
 MIDDLEWARE = [
