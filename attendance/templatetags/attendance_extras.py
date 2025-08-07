d69138442aced61ba5cf955cbfb4d5e79631e08a
SAEMI-wp <saisaisaida12@gmail.com>
2025-08-07 10:39:06 +0900

    Add files via upload

--- attendance/templatetags/attendance_extras.py
+++ attendance/templatetags/attendance_extras.py
@@ -13,4 +13,16 @@ def get_item(lst, index):
     try:
         return lst[index]
     except (IndexError, TypeError):
-        return "" 
+        return ""
+
+@register.filter
+def has_admin_access(user):
+    """사용자가 관리자 접근 권한이 있는지 확인합니다."""
+    if user.is_superuser:
+        return True
+    
+    # 사용자가 속한 그룹 이름들을 확인
+    user_groups = [group.name for group in user.groups.all()]
+    admin_positions = ['사장', '이사', '부장']
+    
+    return any(position in user_groups for position in admin_positions) 
